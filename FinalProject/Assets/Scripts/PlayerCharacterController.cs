c6e2faa7402e82381a4fafcc451ba458aa8ae626
ghostrecollection <heartforbrains126@gmail.com>
2025-04-17 20:20:36 -0400

    Cinemachine Camera Start

--- FinalProject/Assets/Scripts/PlayerCharacterController.cs
+++ FinalProject/Assets/Scripts/PlayerCharacterController.cs
@@ -33,17 +33,93 @@ public class PlayerCharacterController : MonoBehaviour
     public bool isGrounded;
 
 
+   
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Time is true to real world time
+        Time.timeScale = 1f;
+
+        // Grab Character Controller
+        controller = GetComponent<CharacterController>();
+
+        // Lock cursor to middle of screen
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; // Will need to make visible for options/inventory type screens, etc..
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+
+       
+
+        // Movement Axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical"); // zInput gets players w or s input which is -1 or 1
+
+        // Jogging Movement
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            isJogging = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isJogging = false;
+        }
+
+        // Velocity Movement
+        velocity = transform.forward * zInput * currentSpeed + transform.right * horizontalInput * currentSpeed + Vector3.up * velocity.y;
+
     }
+
+    private void MovePlayer()
+    {
+        isGrounded = controller.isGrounded;
+        // If the player is grounded, the gravity resets
+        if (isGrounded)
+        {
+            // Small Downward force to keep the player grounded
+            velocity.y = -1f;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = jumpForce;
+            }
+        }
+        else
+        {
+            // Gravity is applied when player is in the air
+            velocity.y -= gravity * -2f * Time.deltaTime;
+        }
+
+        // Jogging
+        if (isJogging)
+        {
+            currentSpeed = jogSpeed;
+            // Debug.Log("Current Speed Jog:" + currentSpeed);
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+
+        // Move player on the x and z
+        controller.Move(playerMovementInput * currentSpeed * Time.deltaTime);
+        // Debug.Log("Current Speed:" + currentSpeed);
+        // Apply vertical movement (gravity and jumping)
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+  
+
+
+
 }
