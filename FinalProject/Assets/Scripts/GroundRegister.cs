07a00d9ca519f4ba79ec01216b8cd122c4ceb6a2
ghostrecollection <heartforbrains126@gmail.com>
2025-04-29 20:51:33 -0400

    GroundRegister Progress

--- FinalProject/Assets/Scripts/GroundRegister.cs
+++ FinalProject/Assets/Scripts/GroundRegister.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.WebCam;
 
 public class GroundRegister : MonoBehaviour
 {
@@ -11,16 +12,59 @@ public class GroundRegister : MonoBehaviour
 
     // Locational bools to know when the player is where
     [SerializeField] bool onMainPath, onNearPath, onFarFromPath;
+    // Script Reference
+    [SerializeField] PlayerCharacterController playerCharacterControllerScript;
 
+    [SerializeField] Ray ray;
+    [SerializeField] RaycastHit hit;
 
+    [SerializeField] float rayLength = .4f;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCharacterControllerScript = GetComponent<PlayerCharacterController>();
+        Physics.Raycast(playerCharacterControllerScript.groundCheck.position, Vector3.down, rayLength, playerCharacterControllerScript.groundLayer);
+
+        GroundCheck();
     }
+
+
+    // Tracks when player is at certain locations
+    private void GroundCheck()
+    {
+        if (hit.transform.CompareTag("MainPath"))
+        {
+            // TRUE LOCATION
+            onMainPath = true;
+            onNearPath = false;
+            onFarFromPath = false;
+        }
+
+        if (hit.transform.CompareTag("NearPath"))
+        {
+            onMainPath = false;
+            // TRUE LOCATION
+            onNearPath = true;
+            onFarFromPath = false;
+        }
+
+        if (hit.transform.CompareTag("FarFromPath"))
+        {
+            onMainPath = false;
+            onNearPath = false;
+            // TRUE LOCATION
+            onFarFromPath = true;
+        }
+
+
+
+
+    }
+
+
 
     // Update is called once per frame
     void Update()
